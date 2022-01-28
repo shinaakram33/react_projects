10a36b9d2f0b80272fc7491d6f3b96a8e1df547b
Shina Akram <shinaakram72@gmail.com>
2022-01-29 01:20:14 +0500

    React Components,States, lift State-up and Props implemented

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent';
+import {DISHES} from './shared/dishes';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    };
+  }
   render(){
     return (
       <div className="App">
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">ManoSalwa Restaurant</NavbarBrand>
-            <Menu />
+            <Menu dishes={this.state.dishes}/>
           </div>
         </Navbar>
       </div>
